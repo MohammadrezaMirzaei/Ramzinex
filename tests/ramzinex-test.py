dda6756a6ea0b0e28a78569cd773cefb58b0e029
Mirzaei 09369109795 <mirzaeimohammadreza98@gmail.com>
2021-10-09 12:03:24 +0000

    ramzinex_first

---
+++ tests/ramzinex-test.py
@@ -0,0 +1,18 @@
+import unittest
+import python_razminex_api.ramzinex
+
+
+class RamzinexTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.public_api = python_razminex_api.ramzinex.PublicAPI()
+
+    def test_zero(self):
+        """Test get_markets"""
+        # get tether/rial market
+        result = self.public_api.get_markets(11)
+        self.assertEqual(result["status"], 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
